4048c1e7a5050c7bd904fb896d4435b30b7709ca
Ivan Franco <ifranco@tupaca.com>
2019-06-10 15:44:05 -0300

    init test

---
+++ NumberOfDiscIntersectionsTest/NumberOfDiscIntersectionsTest.cs
@@ -0,0 +1,13 @@
+namespace NumberOfDiscIntersectionsTest
+{
+    using NUnit.Framework;
+
+    public class NumberOfDiscIntersectionsTest
+    {
+        [Test]
+        public void WhenArrayIsEmpty_IntersectionsAreZero()
+        {
+            return 0;
+        }
+    }
+}
